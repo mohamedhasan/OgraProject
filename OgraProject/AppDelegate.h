4a143b8209b5a1283f7116700f0e876b12779e50
Mohammed Hasan <mohamedhasan_17@hasan.local>
2012-10-06 21:28:24 +0200

    Initial Commit

---
+++ OgraProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OgraProject
+//
+//  Created by Mohammed Hasan on 10/6/12.
+//  Copyright (c) 2012 Mohammed Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OgraProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OgraProject
+//
+//  Created by Mohammed Hasan on 10/6/12.
+//  Copyright (c) 2012 Mohammed Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
